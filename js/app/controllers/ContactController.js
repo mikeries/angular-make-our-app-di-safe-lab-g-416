32acf27b0ab2467b9fa7d20e74ed4863280a9f78
Michael Ries <michael.ries@gmail.com>
2017-10-28 02:34:01 +0000

    automatically backed up by learn

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,10 +1,12 @@
-function ContactController($scope, $timeout) {
+function ContactController(blah, blah2) {
 	$scope.name = 'Bill Gates';
 
 	$timeout(function () {
 		$scope.name = 'Steve Jobs';
 	}, 5000);
 }
+
+ContactController.$inject = ['$scope', '$timeout'];
 
 angular
 	.module('app')
